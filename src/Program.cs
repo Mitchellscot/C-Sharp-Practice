97362079218fb42a56fddf82c14e357090c08e8a
Mitchell <Mitchellscott@me.com>
2021-05-05 22:15:18 -0500

    string test

--- src/Program.cs
+++ src/Program.cs
@@ -5,18 +5,28 @@ using System.Text;
 using static System.Console;
 using System.Diagnostics;
 
+
 namespace Practice
 {
     class Program
     {
         public static void Main()
         {
-           Func<int, int> square = x => x*x;
-           var z = int.Parse(ReadLine());
-           System.Console.WriteLine(square(z).ToString());
+            WriteLine(IsPangram("The quick brown fox jumps over the lazy dog"));
 
         }
-
+        public static bool IsPangram(string str)
+        {
+            var alphabet = "abcdefghijklmnopqrstuvwxyz";
+            foreach (var letter in alphabet)
+            {
+                if (!str.ToLower().Contains(letter))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
 

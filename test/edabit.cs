19fbad667ff086f5ef804f0fe43854c4370eabd8
Mitchell <Mitchellscott@me.com>
2021-08-02 21:52:46 -0500

    added a few tests and some xml comments

--- test/edabit.cs
+++ test/edabit.cs
@@ -8,16 +8,62 @@ namespace edabit
 {
     public class edabit
     {   
-        //Create a function that computes the number of characters that differ between two strings. 
+        /// <summary>
+        /// Counts how many D's are in a sentence
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static int Method9(string s) => s.Count(x => x == 'D' || x =='d');
+
         [Fact]
-        public void Test6()
+        public void Test9()
         {
-            Assert.Equal(0, Test6Mb("abcde", "abcde"));
-            Assert.Equal(1, Test6Mb("abcde", "absde"));
-            Assert.Equal(2, Test6Mb("abmcdz", "abmsdq"));
-            Assert.Equal(5, Test6Mb("abcde", "bcdef"));
+            Assert.Equal(4, Method9("My friend Dylan got distracted in school."));
+            Assert.Equal(3, Method9("Debris was scattered all over the yard."));
+            Assert.Equal(3, Method9("The rodents hibernated in their den."));
         }
-        public static int Test6M(string a, string b)
+        /// <summary>
+        /// Returns true if an input string contains only uppercase or only lowercase letters.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool Method8(string s)
+        {
+            return s.All(x => Char.IsUpper(x)) || s.All(x => Char.IsLower(x)) ? true : false;
+        }
+
+        [Fact]
+        public void Test8()
+        {
+            Assert.True(Method8("hello"));
+            Assert.True(Method8("HELLO"));
+            Assert.False(Method8("Hello"));
+            Assert.False(Method8("ketcHUp"));
+        }
+        /// <summary>
+        /// Return how many claps are in a sentance
+        /// </summary>
+        /// <param name="claps"></param>
+        /// <returns></returns>
+        public static int Method7(string claps)
+        {
+            return claps.Count(c => c == 'C');
+        }
+
+        [Fact]
+        public void Test7()
+        {
+            Assert.Equal(4, Method7("ClaClaClaClap!"));
+            Assert.Equal(6, Method7("ClClClaClaClaClap!"));
+            Assert.Equal(9, Method7("CCClaClClap!Clap!ClClClap!"));
+        }
+        /// <summary>
+        /// Computes the number of characters that differ between two strings.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int Method6(string a, string b)
         {
             int count = 0;
             for (int i = 0; i < a.Length; i++)
@@ -28,32 +74,48 @@ namespace edabit
             }
             return count;
         }
-        //linq version
-        public static int Test6Mb(string a, string b)
+
+        /// <summary>
+        /// Computes the number of characters that differ between two string - Linq version
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int Method6b(string a, string b) => a.Where((t, x) => t != b[x]).Count();
+
+        [Fact]
+        public void Test6()
         {
-            return a.Where((t, x) => t != b[x]).Count();
+            Assert.Equal(0, Method6("abcde", "abcde"));
+            Assert.Equal(1, Method6("abcde", "absde"));
+            Assert.Equal(2, Method6b("abmcdz", "abmsdq"));
+            Assert.Equal(5, Method6b("abcde", "bcdef"));
         }
-        //Take an array of integers (positive or negative or both) and return the sum of the absolute value of each element.
+
+        /// <summary>
+        /// Take an array of integers (positive or negative or both) and return the sum of the absolute value of each element.
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static int Method5(int[] arr)
+        { 
+            return arr.Select(x => Math.Abs(x)).Sum();
+        }
+
         [Fact]
         public void Test5()
         { 
             int[] testArray = {1, -2, 3, -4, 5};
-            Assert.Equal(15, Test5M(testArray));
+            Assert.Equal(15, Method5(testArray));
         }
-        public static int Test5M(int[] arr)
-        { 
-            return arr.Select(x => Math.Abs(x)).Sum();
-        }
-        [Fact] //returns the smaller number of two
-        public void Test4()
-        {
-            Assert.Equal("5", Test4M("5", "6"));
-            Assert.Equal("5", Test4M("6", "5"));
-            Assert.Equal("9078", Test4M("107838", "9078"));
-            Assert.Equal("9078", Test4Mb("107838", "9078"));
 
-        }
-        public static string Test4M(string a, string b)
+        /// <summary>
+        /// Returns the smaller number of two strings
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static string Method4(string a, string b)
         {
             int c, d;
             if (int.TryParse(a, out c) && int.TryParse(b, out d)){
@@ -61,73 +123,101 @@ namespace edabit
             }
             else return "nope";
         }
-        public static string Test4Mb(string n1, string n2)
+
+        /// <summary>
+        /// Returns the smaller of two strings
+        /// </summary>
+        /// <param name="n1"></param>
+        /// <param name="n2"></param>
+        /// <returns></returns>
+        public static string Method4b(string n1, string n2) => Int32.Parse(n1) > Int32.Parse(n2) ? n2 : n1;
+
+        [Fact]
+        public void Test4()
         {
-            return Int32.Parse(n1) > Int32.Parse(n2) ? n2 : n1;
+            Assert.Equal("5", Method4("5", "6"));
+            Assert.Equal("5", Method4("6", "5"));
+            Assert.Equal("9078", Method4b("107838", "9078"));
+            Assert.Equal("9078", Method4b("107838", "9078"));
         }
-        [Fact] //takes an array of numbers and return both the minimum and maximum numbers
-        public void Test3()
-        {
-            int[] a = {1, 50};
-            int[] b = {6, 1, 45, 50};
-            Assert.Equal(a, Test3M(b));
-            int[] c = {-99, 209};
-            int[] d = {3, 54, -99, 209, 34, 75, -3, -45};
-            Assert.Equal(c, Test3Mb(d));
-        }
-        public static int[] Test3M(int[] arr)
+        
+        /// <summary>
+        /// Takes an array of numbers and return both the minimum and maximum numbers
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static int[] Method3(int[] arr)
         {
             int[] answer = new int[2];
             answer[0] = arr.Min();
             answer[1] = arr.Max();
             return answer;
         }
-        public static int[] Test3Mb(int[] arr)
+        /// <summary>
+        /// Takes an array of numbers and return both the minimum and maximum numbers - with anonymous function
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static int[] Method3b(int[] arr) => new[] {arr.Min(), arr.Max()};
+        
+        [Fact]
+        public void Test3()
         {
-            //using an anonymous function
-            return new[] {arr.Min(), arr.Max()};
+            int[] a = {1, 50};
+            int[] b = {6, 1, 45, 50};
+            Assert.Equal(a, Method3(b));
+            int[] c = {-99, 209};
+            int[] d = {3, 54, -99, 209, 34, 75, -3, -45};
+            Assert.Equal(c, Method3b(d));
         }
 
-        [Fact] //returns true if all characters in a string are identical 
-        public void Test2()
-        {
-            Assert.True(Test2Ma("aaaaa"));
-            Assert.False(Test2Mb("aaabaaa"));
-            Assert.False(Test2Mc("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmn"));
-            Assert.False(Test2Mc("nmmmmmmmmm"));
-            Assert.True(Test2Mc("z"));
-        }
-
-        public static bool Test2Ma(string s)
+        /// <summary>
+        /// Returns true if all characters in a string are identical
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool Method2(string s)
         {
             for (int i = 0; i < s.Length; i++)
             {
-                if (s[0] != s[i]){  return false; }
+                if (s[0] != s[i])
+                {  
+                    return false; 
+                }
             }
             return true;
         }
-        public static bool Test2Mb(string s)
+
+        /// <summary>
+        /// Returns true if all characters in a string are identical - Linq version 1
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool Method2b(string s) => s.All(x => x == s[0]);
+
+        /// <summary>
+        /// Returns true if all characters in a string are identical - Linq version 2
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool Method2c(string s) => s.Distinct().Count() == 1;
+
+        [Fact]
+        public void Test2()
         {
-            return s.All(x => x == s[0]);
-        }
-        public static bool Test2Mc(string s)
-        {
-            //with linq, Distinct removes all that are duplicates, 
-            //Count returns number in a collection
-            //I suppose the equals makes it return a boolean
-            return s.Distinct().Count() == 1;
+            Assert.True(Method2("aaaaa"));
+            Assert.False(Method2b("aaabaaa"));
+            Assert.False(Method2c("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmn"));
+            Assert.False(Method2c("nmmmmmmmmm"));
+            Assert.True(Method2c("z"));
         }
 
-        [Fact] //takes a number (from 1 to 12) and returns its corresponding month name as a string
-        public void Test1()
-        {
-            Assert.Equal("April", Test1M(4));
-            Assert.Equal("November", Test1M(11));
-            Assert.Equal("December", Test1M(12));
-            Assert.Equal("Not on the Calendar", Test1M(13));
-
-        }
-        public static string Test1M(sbyte num)
+        /// <summary>
+        /// Takes a number (from 1 to 12) and returns its corresponding month name as a string
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static string Method1(sbyte num)
         {
             return num switch
                 {
@@ -145,6 +235,15 @@ namespace edabit
                     12 => "December",
                     _ => "Not on the Calendar"
                 };            
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal("April", Method1(4));
+            Assert.Equal("November", Method1(11));
+            Assert.Equal("December", Method1(12));
+            Assert.Equal("Not on the Calendar", Method1(13));
         }
     }
 }
